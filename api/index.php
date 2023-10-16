a62112bcc9bf9f64ec81c53b0d66311da73a3a3b
Bandit Silachai <mbandit001@yahoo.co.th>
2023-10-16 15:14:08 +0700

    🔨 fix(api): add header options

--- api/index.php
+++ api/index.php
@@ -7,6 +7,18 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+  });
+  
+  $app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+      ->withHeader('Access-Control-Allow-Origin', '*')
+      ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+      ->withHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  });
  
 $app->get("/", function (Request $request, Response $response, $args) {
     $response->getBody()->write("<h1>Hello World</h1>");
